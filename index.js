5731aa59404e42dbaded135b23bdc41ac855cfb7
florenciaPenaSepulveda <florencia.pena@alumnos.ucn.cl>
2025-06-29 11:43:38 -0400

    feat: Agrega nueva ruta para demostracion CI/CD

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.send('¡Hola desde mi aplicación CI/CD!');
 });
 
+app.get('/nueva-ruta', (req, res) => {
+  res.send('¡Esta es la nueva ruta, desplegada con CI/CD!');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
